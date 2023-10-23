507bbf297a71abe301784aff4074e9af26081ff4
Amelia Esra <ameliaesras@gmail.com>
2023-10-23 11:21:18 +0700

    Apply ImageAssetHelper

---
+++ eTagApp/ImageAssetHelper.swift
@@ -0,0 +1,113 @@
+//
+//  ImageAssetHelper.swift
+//  eTagApp
+//
+//  Created by PT Phincon on 19/10/23.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+import Alamofire
+
+public final class ImageAssetsCache: NSObject {
+    
+    private override init() {}
+    
+    public static func configCache(cache: ImageCache) -> ImageCache {
+        // reference: https://github.com/onevcat/Kingfisher/issues/1584
+        // Limit memory cache size to 250 MB and check memory clean up every 10 seconds.
+        cache.memoryStorage.config = MemoryStorage.Config.init(
+            totalCostLimit: 1024 * 1024 * 250,
+            cleanInterval: 100
+        )
+        // Memory image expires after 1 minute.
+        cache.memoryStorage.config.expiration = .seconds(60)
+        cache.diskStorage.config.expiration = .seconds(60)
+        return cache
+    }
+}
+
+public final class ImageAssetHelper: NSObject {
+    
+    private override init() { }
+    
+    static var storedEtag : String = ""
+    static var headers : HTTPHeaders = [:]
+    
+    
+    // MARK: - Method
+    
+    public static func fetchImageIn2(_ imageView: UIImageView, url: String?, placeholder: UIImage?) {
+        
+        guard let url = url else {
+            // Default Image
+            imageView.image = placeholder
+            return
+        }
+        
+        // Check valid URL string
+        if let link = URL(string: url) {
+            // Add regular UIImageView
+            let options = getKingfisherOptions(url: link, withEtag: true)
+            imageView.kf.setImage(with: link, options: options, completionHandler: { (result) in
+                switch result {
+                case .success(let value):
+                    if value.cacheType == .none {
+                        getEtagForWcmsAsset(wcmsAssetUrl: url)
+                        print("✅ Image loaded from network.")
+                    } else if value.cacheType == .memory {
+                        print("✅ Image loaded from cache.")
+                    }
+                    break
+                case .failure(let error):
+                    print("✅ error fetchImage: \(error)")
+                    print("✅ imgURL: \(link)")
+                    imageView.kf.setImage(with: link, options: nil)
+                    imageView.layoutIfNeeded()
+                    // Default Image
+                    imageView.image = placeholder
+                }
+            })
+        } else {
+            
+            // Default Image
+            imageView.image = placeholder
+        }
+    }
+    
+    private static func getKingfisherOptions(url: URL, withEtag: Bool = false) -> KingfisherOptionsInfo {
+        let cache = ImageAssetsCache.configCache(cache: ImageCache(name: url.absoluteString))
+        var options: KingfisherOptionsInfo = []
+        
+        if withEtag {
+            print("✅ storedEtag: \(storedEtag)")
+            let requestModifier = MyRequestModifier(etag: storedEtag)
+            options = [.forceRefresh, .requestModifier(requestModifier)]
+        } else {
+            options = []
+        }
+      
+        options.append(.targetCache(cache))
+        return options
+    }
+    
+    //MARK: ALAMOFIRE ASSET
+    public static func getEtagForWcmsAsset(wcmsAssetUrl: String = "") {
+        AF.request(wcmsAssetUrl, method: .head, headers: headers)
+            .validate(statusCode: 200..<300)
+            .response { response in
+            switch response.result {
+            case .success:
+                if let etag = response.response?.allHeaderFields["Etag"] as? String {
+                    self.storedEtag = etag
+                    print("✅ Image AF loaded from network \(etag)")
+                }
+                break
+            case .failure(let error):
+                print("✅ Request failed with error: \(error)")
+                break
+            }
+        }
+    }
+}
--- eTagApp/ViewController.swift
+++ eTagApp/ViewController.swift
@@ -25,56 +25,75 @@ class ViewController: UIViewController, ImageDownloaderDelegate {
     var storedETag: String?
     
     // Your image URL and headers
-    let imageURL = URL(string: "https://tdwstcontent.telkomsel.com/sites/default/files/images/pages/assets/background-min.png")!
+    let imageURL = "https://tdwstcontent.telkomsel.com/sites/default/files/images/pages/assets/background-min.png"
     var headers: HTTPHeaders = [:]
+    
+    let downloader = KingfisherManager.shared.downloader  // Or another downloader if you are not using the default one.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.imageSourceLabel.text = ""
         self.KingfisherCacheLabel.text = ""
+//        downloader.delegate = self
     }
     
-    func getImageRequest() {
-        AF.request(imageURL, method: .head, headers: headers)
-            .validate(statusCode: 200..<300)
-            .response { response in
-                switch response.result {
-                case .success:
-                    if let etag = response.response?.allHeaderFields["Etag"] as? String {
-                        self.storedETag = etag
-                        self.imageSourceLabel.text = "Image AF loaded from network"
-                    }
-                case .failure(let error):
-                    print("✅ Request failed with error: \(error)")
-                }
-            }
-    }
+//    func getImageRequest() {
+//        AF.request(imageURL, method: .head, headers: headers)
+//            .validate(statusCode: 200..<300)
+//            .response { response in
+//                switch response.result {
+//                case .success:
+//                    if let etag = response.response?.allHeaderFields["Etag"] as? String {
+//                        self.storedETag = etag
+//                        self.imageSourceLabel.text = "Image AF loaded from network"
+//                    }
+//                case .failure(let error):
+//                    print("✅ Request failed with error: \(error)")
+//                }
+//            }
+//    }
     
-    func loadImageWithKingfisher() {
-        let etag = storedETag ?? ""
-        let requestModifier = MyRequestModifier(etag: etag)
-            let options: KingfisherOptionsInfo = [.forceRefresh, .requestModifier(requestModifier)]
-
-            bgImage.kf.setImage(with: imageURL, options: options, completionHandler: { result in
-                switch result {
-                case .success(let value):
-                    // Image loaded successfully
-                    if value.cacheType == .none {
-                        self.getImageRequest()
-                        print("✅ Image loaded from network.")
-                        self.KingfisherCacheLabel.text = "Image KF loaded from network"
-                    } else if value.cacheType == .memory {
-                        print("✅ Image loaded from cache.")
-                        self.KingfisherCacheLabel.text = "Image KF loaded from cache"
-                    }
-                case .failure(let error):
-                    print("✅ error: \(error)")
-                    self.bgImage.kf.setImage(with: self.imageURL, options: nil)
-                    self.KingfisherCacheLabel.text = "Image KF loaded from cache"
-                    self.imageSourceLabel.text = ""
-                }
-            })
-    }
+//    func imageDownloader(_ downloader: ImageDownloader, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+//           // Check if the response is an HTTP response (in case of non-HTTP responses)
+//           guard let httpResponse = response as? HTTPURLResponse else {
+//               completionHandler(.allow)
+//               return
+//           }
+//
+//           if let etag = httpResponse.allHeaderFields["Etag"] as? String {
+//               self.storedETag = etag
+//               print("✅ etag inside imageDownloader: \(etag)")
+//           }
+//
+//           // Allow the response to proceed
+//           completionHandler(.allow)
+//    }
+    
+//    func loadImageWithKingfisher() {
+//        let etag = storedETag ?? ""
+//        let requestModifier = MyRequestModifier(etag: etag)
+//            let options: KingfisherOptionsInfo = [.forceRefresh, .requestModifier(requestModifier)]
+//
+//            bgImage.kf.setImage(with: imageURL, options: options, completionHandler: { result in
+//                switch result {
+//                case .success(let value):
+//                    // Image loaded successfully
+//                    if value.cacheType == .none {
+////                        self.getImageRequest()
+//                        print("✅ Image loaded from network.")
+//                        self.KingfisherCacheLabel.text = "Image KF loaded from network"
+//                    } else if value.cacheType == .memory {
+//                        print("✅ Image loaded from cache.")
+//                        self.KingfisherCacheLabel.text = "Image KF loaded from cache"
+//                    }
+//                case .failure(let error):
+//                    print("✅ error: \(error)")
+//                    self.bgImage.kf.setImage(with: self.imageURL, options: nil)
+//                    self.KingfisherCacheLabel.text = "Image KF loaded from cache"
+//                    self.imageSourceLabel.text = ""
+//                }
+//            })
+//    }
     
     func clearKingfisherCache() {
         KingfisherManager.shared.cache.clearCache()
@@ -83,7 +102,8 @@ class ViewController: UIViewController, ImageDownloaderDelegate {
     }
     
     @IBAction func hitAPIButtonTapped(_ sender: UIButton) {
-        loadImageWithKingfisher()
+//        loadImageWithKingfisher()
+        ImageAssetHelper.fetchImageIn2(bgImage, url: imageURL, placeholder: nil)
     }
     
     @IBAction func clearCaheButtonTapped(_ sender: Any) {
